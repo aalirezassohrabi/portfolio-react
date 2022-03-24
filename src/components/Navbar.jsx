1264ca20b2e490942be2a381afd3fb419ce42e2b
Alireza Sohrabi <myregularmail21@gmail.com>
2022-03-24 23:23:29 +0430

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,21 @@
-import { FaBars, FaFacebook, FaGithub, FaLinkedin, FaTimes } from 'react-icons/fa'
-import React,{useState} from 'react'
+import {
+  FaBars,
+  FaFacebook,
+  FaGithub,
+  FaLinkedin,
+  FaTimes,
+} from 'react-icons/fa'
+import React, { useState } from 'react'
 
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import { HiOutlineMail } from 'react-icons/hi'
+import { Link } from 'react-scroll'
 import Logo from '../assets/logo.png'
 
 const Navbar = () => {
-const [nav,setNav] = useState(false)
-const handleClick = () => setNav(!nav)
- 
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
@@ -17,11 +24,32 @@ const handleClick = () => setNav(!nav)
       {/* menu */}
 
       <ul className='hidden md:flex'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Work</li>
-        <li>Contact</li>
+        <li>
+          <Link to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          {' '}
+          <Link to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to='work' smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/* Hamburger */}
@@ -36,11 +64,32 @@ const handleClick = () => setNav(!nav)
             : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
         }
       >
-        <li className='py-6 text-4xl'>Home</li>
-        <li className='py-6 text-4xl'>About</li>
-        <li className='py-6 text-4xl'>Skills</li>
-        <li className='py-6 text-4xl'>Work</li>
-        <li className='py-6 text-4xl'>Contact</li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='work' smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/* social icons */}
@@ -67,7 +116,8 @@ const handleClick = () => setNav(!nav)
               className='flex justify-between items-center w-full text-gray-300'
               href='/'
             >
-              Email<HiOutlineMail size={30} />
+              Email
+              <HiOutlineMail size={30} />
             </a>
           </li>{' '}
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
